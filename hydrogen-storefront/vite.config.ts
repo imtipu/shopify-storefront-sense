61faee834ba99dfc41e8807da6a6079b2155eb1a
Borhan Tipu <imtipu.dev@gmail.com>
2026-01-04 06:27:05 +0600

    npm update

--- hydrogen-storefront/vite.config.ts
+++ hydrogen-storefront/vite.config.ts
@@ -4,6 +4,8 @@ import {oxygen} from '@shopify/mini-oxygen/vite';
 import {reactRouter} from '@react-router/dev/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
+import {vercelPreset} from '@vercel/remix/vite';
+
 
 export default defineConfig({
   plugins: [
